3cc4dc754d00e33cdf70991701ab1d02ddb2615e
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-09 21:03:05 +0530

    changed type of button and added a eventlistner for button

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const textArea = document.querySelector(".textarea");
 const button = document.querySelector("button");
+
+button.addEventListener("click", addTodoListItem());
+function addTodoListItem(){
+
+}
