fe8c5fd09ba4c61e7dc5a72f853a121027f22a0f
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-10 12:55:51 +0530

    created div and a p to store the data of todo list

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const textArea = document.querySelector(".textarea");
 const button = document.querySelector("button");
 
-button.addEventListener("click", addTodoListItem());
+button.addEventListener("click", addTodoListItem);
 function addTodoListItem(){
-
+    
+    const toDoDiv = document.createElement("div");
+    const item =document.createElement('p');
 }
