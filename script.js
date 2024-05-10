b91ca89a72ab6084685ab32f34b464f8e810a884
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-10 14:50:34 +0530

    added delete button and css to delete button

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function addTodoListItem(){
     item.innerHTML = textArea.value;
     toDoDiv.appendChild(item);
     const deleteButton = document.createElement("button");
+    deleteButton.classList.add("deleteButton");
     deleteButton.innerHTML = "Delete";
     toDoDiv.appendChild(deleteButton);
     todoList.appendChild (toDoDiv)
